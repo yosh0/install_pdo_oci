ce3ea93e0362b5748bf18900b58e9a56b70b9d0f
yosh0 <yosh0@users.noreply.github.com>
2019-03-26 07:58:20 +0700

    Update install-php-pdo-oci_debian_10.sh

--- install-php-pdo-oci_debian_10.sh
+++ install-php-pdo-oci_debian_10.sh
@@ -1,1 +1,5 @@
 
+ln -sf /opt/oracle/instantclient_12_1/libipc1.so /lib/x86_64-linux-gnu/libipc1.so
+ln -sf /opt/oracle/instantclient_12_1/libmql1.so /lib/x86_64-linux-gnu/libmql1.so
+ln -sf /opt/oracle/instantclient_12_1/libnnz12.so /lib/x86_64-linux-gnu/libnnz12.so
+ln -sf /opt/oracle/instantclient_12_1/libons.so /lib/x86_64-linux-gnu/libons.so
